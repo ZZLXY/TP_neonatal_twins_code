6f8965c8d80e107ccbeb1bbff0ee63e2995ba0c5
BrainResearcher <85658420+ZZLXY@users.noreply.github.com>
2024-07-18 06:40:12 -0400

    Update maskregistration_MIN152_to_extdhcp40wk.sh

--- A_transform_ROI_to_dhcp_space/maskregistration_MIN152_to_extdhcp40wk.sh
+++ A_transform_ROI_to_dhcp_space/maskregistration_MIN152_to_extdhcp40wk.sh
@@ -43,31 +43,32 @@ flirt -in MASKS/${tempimg}_mask.nii -applyisoxfm 0.5 -ref MASKS/${tempimg}_mask.
 mri_binarize --i MASKS/upsample_${tempimg}_mask.nii.gz --min 0.1 --o MASKS/bi_upsample_${tempimg}_mask.nii.gz
 
 
-# negate the mask
+###negate the mask
 
 ImageMath 3 MASKS/${tempimg}_neg.img Neg MASKS/bi_upsample_${tempimg}_mask.nii.gz
 
-## normalize the negative mask
-## use antsRegistrationSyNQuick.sh -d 3 -f /template_t2.nii -m colin27_t1_tal_hires_brain.nii -o colin2newborn to generate the transformation matrix
+
+###normalize the negative mask
 
 antsApplyTransforms -d 3 -i MASKS/${tempimg}_neg.img -o MASKS/neg_deform_${pre}${tempimg}_mask.nii -r week40_T2w.nii.gz -t dhcp40wk_to_extdhcp40wk_warp.nii.gz -t ICBM1522newborn1Warp.nii.gz -t ICBM1522newborn0GenericAffine.mat
 
-## negate back to the intended mask
+
+###negate back to the intended mask
 
 ImageMath 3 MASKS/${pre}_deform_${tempimg}_mask.nii Neg MASKS/neg_deform_${pre}${tempimg}_mask.nii
 
 
-##need to generate a mask cutting part of brain stem, so no border voxels were included
+###generate a mask cutting part of brain stem, so no border voxels were included
 
 fslmaths MASKS/${pre}_deform_${tempimg}_mask.nii -mas week40_T2w_y2_z18.nii MASKS/correct_${pre}_deform_${tempimg}.nii.gz
 
 
-## reslice to functional space
+###reslice the mask to match it with the functional images
 
 flirt -in MASKS/correct_${pre}_deform_${tempimg}.nii.gz -applyisoxfm 2.132353 -ref week40_T2w_downsamplefunc.nii.gz -out MASKS/func_correct_mask_${pre}_deform_${tempimg}.nii -init $FSLDIR/etc/flirtsch/ident.mat
 
 
-## binarize the mask
+###binarize the mask
 
 mri_binarize --i MASKS/func_correct_mask_${pre}_deform_${tempimg}.nii.gz --min 0.5 --o MASKS/bi_func_correct_mask_${pre}_deform_${tempimg}.nii.gz
 
