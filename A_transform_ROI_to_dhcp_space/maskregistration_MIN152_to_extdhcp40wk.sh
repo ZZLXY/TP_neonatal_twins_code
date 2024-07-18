024ba9fe805cf4552b0c451ffb9617d4e42733e5
BrainResearcher <85658420+ZZLXY@users.noreply.github.com>
2024-07-18 06:35:28 -0400

    Update maskregistration_MIN152_to_extdhcp40wk.sh

--- A_transform_ROI_to_dhcp_space/maskregistration_MIN152_to_extdhcp40wk.sh
+++ A_transform_ROI_to_dhcp_space/maskregistration_MIN152_to_extdhcp40wk.sh
@@ -5,24 +5,23 @@
 #The extended dHCP volumetric atlas and transformation matrix can be download from:https://git.fmrib.ox.ac.uk/seanf/dhcp-resources/-/blob/master/docs/dhcp-augmented-volumetric-atlas-extended.md#download-42-gb
 
 #We used a two-step registration approach: we first transformed the adult ROIs to the template_t2 space (dhcp release 2), which were then transformed to the extdhcp40wk space (release 3) using the transformation matrix provided in the third release.
-#Careful inspection revealed that the ROI transformed to the newborn space was more accurate using this method.
+#Careful inspections revealed optimal registration outcomes using this approach. 
 
-#codes used to generate the registration matrix for transform atlas in MNI152 09a space into template t2 space (dhcp release 2)
+#Codes used to generate the transformation matrix from the MNI152 09a space to the template t2 space (dhcp release 2):
 #antsRegistrationSyNQuick.sh -d 3 -f template_t2.nii -m mni_icbm152_t1_tal_nlin_asym_09a_brain.nii -o ICBM1522newborn
 
 
-##used codes to normalize ICBM152 to newborn spaces
+##Codes used to normalize ICBM152 to newborn spaces
 #antsApplyTransforms -d 3 -i mni_icbm152_t1_tal_nlin_asym_09a_brain.nii -o mni_icbm152_t1_tal_nlin_asym_09a_brain_2extdhcp40wk.nii -r week40_T2w.nii.gz -t dhcp40wk_to_extdhcp40wk_warp.nii.gz -t ICBM1522newborn1Warp.nii.gz -t ICBM1522newborn0GenericAffine.mat
 
 
-# Read list of subjects.
+# Read list of ROIs.
 
 
 
 images=(
 'TP'
 'Name_of_your_ROIs'
-
 );
 
 cd /to_your_path
@@ -30,22 +29,18 @@ cd /to_your_path
 pre="ICBM152"
 
 
-
-
-
-for ((i=0;i<=23;i++))
+for ((i=0;i<=1;i++))
 
 
 do
 
 tempimg=${images[i]};
 
-###upsample brain mask to match with week40_T2w.nii.gz
+###upsample the brain mask to match it with week40_T2w.nii.gz
 
 flirt -in MASKS/${tempimg}_mask.nii -applyisoxfm 0.5 -ref MASKS/${tempimg}_mask.nii -out MASKS/upsample_${tempimg}_mask.nii -init $FSLDIR/etc/flirtsch/ident.mat
 
 mri_binarize --i MASKS/upsample_${tempimg}_mask.nii.gz --min 0.1 --o MASKS/bi_upsample_${tempimg}_mask.nii.gz
-
 
 
 # negate the mask
@@ -54,7 +49,6 @@ ImageMath 3 MASKS/${tempimg}_neg.img Neg MASKS/bi_upsample_${tempimg}_mask.nii.g
 
 ## normalize the negative mask
 ## use antsRegistrationSyNQuick.sh -d 3 -f /template_t2.nii -m colin27_t1_tal_hires_brain.nii -o colin2newborn to generate the transformation matrix
-
 
 antsApplyTransforms -d 3 -i MASKS/${tempimg}_neg.img -o MASKS/neg_deform_${pre}${tempimg}_mask.nii -r week40_T2w.nii.gz -t dhcp40wk_to_extdhcp40wk_warp.nii.gz -t ICBM1522newborn1Warp.nii.gz -t ICBM1522newborn0GenericAffine.mat
 
