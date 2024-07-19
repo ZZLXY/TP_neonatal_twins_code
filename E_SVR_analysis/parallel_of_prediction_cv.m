4092e36abcb79a12079265bb6f39624e45c15e56
BrainResearcher <85658420+ZZLXY@users.noreply.github.com>
2024-07-19 11:13:33 +0800

    Update parallel_of_prediction_cv.m

--- E_SVR_analysis/parallel_of_prediction_cv.m
+++ E_SVR_analysis/parallel_of_prediction_cv.m
@@ -1,17 +1,17 @@
-% Install the libsvm-3.22 toolbox (Chang & Lin, 2011) before running the script 
-
+% Install the libsvm-3.22 toolbox (Chang & Lin, 2011) before running this script 
 addpath(genpath('XXX/libsvm-3.22'));
+
 load('your_data_file.mat');% Read you data files
 [nTask, ~] = size(sigpair);
 for iterationno=1:nTask 
-    performanceinex=sigpair(iterationno,2); 
+    performanceindex=sigpair(iterationno,2); 
     
-    tempnanindex=find(isnan(Activity_3Subdivision(:,performanceinex))); 
-    tempperformance=Activity_3Subdivision(:,performanceinex);  %Find the variable that is currently being predicted
+    tempnanindex=find(isnan(Activity_3Subdivision(:,performanceindex))); 
+    tempperformance=Activity_3Subdivision(:,performanceindex);  %Find the variable that is currently being predicted
 
     tempperformance(tempnanindex')=[];
     
-    tempR2R_region=FCPattern_3Subdivision{sigpair(iterationno,1)}; %Find the feature matrix that will be used
+    tempR2R_region=FCPattern_3Subdivision{sigpair(iterationno,1)}; %Find the feature matrix that is currently being used
     tempR2R_region(tempnanindex',:)=[]; 
 
     tempN=N-length(tempnanindex);
@@ -20,14 +20,12 @@ for iterationno=1:nTask
 
    for shuffleno=1:10000
        % Shuffle the data based on pre-generated random numbers
-       tempregion_specfic_datashuffle=tempregion_specfic_data(subseq{1,subi}(:,shuffleno),:);
-       tempperformanceshuffle=tempperformance(subseq{1,subi}(:,shuffleno));
+       tempregion_specfic_datashuffle=tempregion_specfic_data(subseq(:,shuffleno),:);
+       tempperformanceshuffle=tempperformance(subseq(:,shuffleno));
 
-       [performance_fc_pearson(iterationno,shuffleno), performance_fc_pearson_p(iterationno,shuffleno)],...
-           =SVR_continuous_label_normalization(tempregion_specfic_data, tempperformance, 'Normalize');
+       [performance_fc_pearson(iterationno,shuffleno), performance_fc_pearson_p(iterationno,shuffleno)]=SVR_continuous_label_normalization(tempregion_specfic_data, tempperformance, 'Normalize');
 
-       [performance_fc_pearson_permutation(iterationno,shuffleno), performance_fc_pearson_p_permutation(iterationno,shuffleno)],...
-           =SVR_continuous_label_normalization(tempregion_specfic_data, tempperformanceshuffle, 'Normalize');
+       [performance_fc_pearson_permutation(iterationno,shuffleno), performance_fc_pearson_p_permutation(iterationno,shuffleno)]=SVR_continuous_label_normalization(tempregion_specfic_data, tempperformanceshuffle, 'Normalize');
 
        clear tempregion_specfic_datashuffle tempperformanceshuffle;
    end
